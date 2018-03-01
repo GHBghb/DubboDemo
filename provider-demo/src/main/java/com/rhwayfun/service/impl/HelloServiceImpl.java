0621fb13135bdbac0525581480a80bb7341a0153
GHBghb <2978032936@qq.com>
2018-03-01 11:02:51 +0800

    Update HelloServiceImpl.java

--- provider-demo/src/main/java/com/rhwayfun/service/impl/HelloServiceImpl.java
+++ provider-demo/src/main/java/com/rhwayfun/service/impl/HelloServiceImpl.java
@@ -2,14 +2,7 @@ package com.rhwayfun.service.impl;
 
 import com.rhwayfun.service.HelloService;
 
-/**
- * 
- * @ClassName: HelloServiceImpl 
- * @Description: TODO
- * @author ZhongCB
- * @date 2016年8月5日 下午5:12:00 
- *
- */
+
 public class HelloServiceImpl implements HelloService {
 
 	public String getName() {
